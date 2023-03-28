e10c70ae968810d34d4eb459c08f8eea2946d57b
Zahar Zubik <zaharzubik@gmail.com>
2023-03-28 02:02:54 +0200

    Update zad3.py

--- C_4/ZAD_3/zad3.py
+++ C_4/ZAD_3/zad3.py
@@ -1,15 +1,20 @@
 def from_big_to_lower (data):
+    print("▓▓▓▓▓▓▓▓▓▓▓▓▓ Start from_big_to_low ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
     for i in range(0, len(data)):
         if data[i] != data[i].lower():
+            test = data[i]
             data[i] = data[i].lower()
-            print(data[i])
+
+            print("Zmieniono literę {} na {}".format(test, data[i]))
     return data
 
 def change_only (data, to_change):
+    print("▓▓▓▓▓▓▓▓▓▓▓▓▓ Start change_only w zakresie {} ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓".format(to_change))
     for i in range(0, len(data)):
         if data[i] == to_change[0]:
+            test = data[i]
             data[i] = to_change[1]
-            print(data[i])
+            print("Zamieniono literę {} na {}".format(test, data[i]))
     return data
 def rozbij_na_liste (data):
     data = list(data)
@@ -19,6 +24,7 @@ def rozbij_na_liste (data):
 def enigma(message, przesun, *to_change):
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                 'v', 'w', 'x', 'y', 'z']
+
     shift = przesun % len(alphabet)
     if shift == 0 :
         shift = przesun
@@ -38,7 +44,11 @@ def enigma(message, przesun, *to_change):
     for c in to_change:
         abc = change_only(abc, c)
 
+    print("▓▓▓▓▓▓▓▓▓▓▓▓▓ Start enigma z przesunięciem: {} dla tekstu \"{}\" ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓".format(przesun, message))
     return ''.join(abc)
+
+
+
 
 # Tu zmieniamy STRINGA
 my_string = "The Project Gutenberg eBook of Alice’s Adventures in Wonderland, by Lewis Carroll!"
